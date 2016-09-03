d982a90d8b0e8076e0591870144644ee6daeabcb
DESKTOP-6UCVEPV\dun <313159511@qq.com>
2016-09-04 06:52:51 +0800

    增加Unity示例工程

---
+++ AssetBundle-master/Assets/Scripts/Test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using LYAssetFrameWork;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+
+    public Image image;
+    public Canvas canvas;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    LYAssetManager.FolderPath = Application.streamingAssetsPath;
+	    LYAssetManager.ManifestName = "StreamingAssets";
+	   
+        LYAssetManager.Instance.LoadAssetBundleAsyn("testprefab", (LYAssetBundle bundle) =>
+        {
+            var obj = bundle.assetBundle.LoadAsset<GameObject>("Panel");
+            GameObject temp = Instantiate(obj);
+            temp.transform.SetParent(canvas.transform, false);
+            temp.transform.localPosition = Vector3.zero;
+        });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
